c5bcc590c605e5d07b241cb942fec701795e42d8
Piotrek Koszuliński <pkoszulinski@gmail.com>
2022-12-16 15:35:30 +0100

    Other: Make Heading 1 mean h1 and enabled h4. Closes #28. Closes #21.

--- admin/src/components/CKEditorInput/Configurator.js
+++ admin/src/components/CKEditorInput/Configurator.js
@@ -56,6 +56,15 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
       '|',
       'undo', 'redo'
     ],
+    heading: {
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+        { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+      ]
+    },
     image: {
       toolbar: [
         'imageStyle:inline',
@@ -116,6 +125,15 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
         '|',
         'undo', 'redo'
     ],
+    heading: {
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+        { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+      ]
+    },
     image: {
       toolbar: [
         'imageStyle:inline',
@@ -192,6 +210,15 @@ const CKEDITOR_BASE_CONFIG_FOR_PRESETS = {
         '|',
         'undo', 'redo'
     ],
+    heading: {
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+        { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+      ]
+    },
     image: {
       resizeUnit: "%",
       resizeOptions: [ {
