ea0d729bfcbd023c0975ed27d2027a86376e9474
mgsy <kacpertomporowski@gmail.com>
2024-08-26 13:30:06 +0200

    Changed the CDN assets injection point.

--- admin/src/components/CKEditorInput/index.jsx
+++ admin/src/components/CKEditorInput/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { Flex } from '@strapi/design-system';
 import { Field } from '@strapi/design-system';
--- admin/src/index.jsx
+++ admin/src/index.jsx
@@ -13,21 +13,6 @@ const IconBox = styled( Flex )`
   }
 `;
 
-if ( !document.querySelector( '#ckeditor5-cdn-script' ) ) {
-  const script = document.createElement( 'script' );
-  const link = document.createElement( 'link' );
-
-  script.src = "https://cdn.ckeditor.com/ckeditor5/43.0.0/ckeditor5.umd.js";
-  script.id = 'ckeditor5-cdn-script'
-
-  link.rel = 'stylesheet';
-  link.href = 'https://cdn.ckeditor.com/ckeditor5/43.0.0/ckeditor5.css';
-  link.id = 'ckeditor5-cdn-styles';
-
-  document.head.appendChild( script );
-  document.head.appendChild( link );
-}
-
 export default {
   register( app ) {
     app.customFields.register( {
@@ -50,7 +35,12 @@ export default {
         defaultMessage: 'The rich text editor for every use case'
       },
       components: {
-        Input: async () => import( './components/CKEditorInput' )
+        Input: async () => {
+          // Inject CKEditor 5 and stylesheet from CDN
+          injectAssetsFromCDN();
+
+          return import( './components/CKEditorInput' )
+        }
       },
       options: {
         base: [
@@ -196,3 +186,21 @@ export default {
     return Promise.resolve( importedTrads );
   },
 };
+
+function injectAssetsFromCDN() {
+  if ( !document.querySelector( '#ckeditor5-cdn-script' ) ) {
+    const script = document.createElement( 'script' );
+    const link = document.createElement( 'link' );
+
+    script.src = "https://cdn.ckeditor.com/ckeditor5/43.0.0/ckeditor5.umd.js";
+    script.async = true;
+    script.id = 'ckeditor5-cdn-script'
+
+    link.rel = 'stylesheet';
+    link.href = 'https://cdn.ckeditor.com/ckeditor5/43.0.0/ckeditor5.css';
+    link.id = 'ckeditor5-cdn-styles';
+
+    document.body.appendChild( script );
+    document.head.appendChild( link );
+  }
+}
