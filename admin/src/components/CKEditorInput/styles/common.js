51f2e77c40e841f719bebe274e3b3c52aabd7f30
kostaszazim <kostas_zazim@yahoo.gr>
2024-11-20 11:52:45 +0200

    fixed source editing height

--- admin/src/components/CKEditorInput/styles/common.js
+++ admin/src/components/CKEditorInput/styles/common.js
@@ -119,6 +119,9 @@ export const style = css`
         transition-duration: 0.2s;
       }
     }
+    .ck-source-editing-area {
+      height: 150px;
+    }
     .ck-source-editing-area textarea {
       font-size: 1.2rem;
       overflow: auto;
