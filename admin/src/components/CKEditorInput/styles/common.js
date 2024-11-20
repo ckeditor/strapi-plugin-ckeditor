0238df857f6195adc8a0d40ff46b789dca23a3a2
kostaszazim <kostas_zazim@yahoo.gr>
2024-11-20 10:19:20 +0200

    added source editing

--- admin/src/components/CKEditorInput/styles/common.js
+++ admin/src/components/CKEditorInput/styles/common.js
@@ -119,5 +119,9 @@ export const style = css`
         transition-duration: 0.2s;
       }
     }
+    .ck-source-editing-area textarea {
+      font-size: 1.2rem;
+      overflow: auto;
+    }
   }
 `;
